ad86aadf38c470b4558310a1d4166c1b5a4f8f8d
tobymohr <tobiasmohr22@gmail.com>
2016-11-15 13:34:36 +0100

    HotkeyTable

---
+++ MathEdit/HotkeyMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathEdit
+{
+    /// <summary>
+    /// Interaction logic for HotkeyMenu.xaml
+    /// </summary>
+    public partial class HotkeyMenu : UserControl
+    {
+        public HotkeyMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void fraction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
