d6d828aa874c1b297d9e5ac1414b0f32f90cb740
Felza <Pet_nysen@yahoo.dk>
2016-11-30 21:48:33 +0100

    Zoom 100% done

---
+++ MathEdit/Helpers/MouseWheelGesture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MathEdit.Helpers
+{
+    public enum MouseWheelDirection { Up, Down }
+
+    class MouseWheelGesture : MouseGesture
+    {
+        public MouseWheelDirection Direction { get; set; }
+
+        public MouseWheelGesture(ModifierKeys keys, MouseWheelDirection direction): base(MouseAction.WheelClick, keys)
+        {
+            Direction = direction;
+        }
+
+        public override bool Matches(object targetElement, InputEventArgs inputEventArgs)
+        {
+            if (!(inputEventArgs is MouseWheelEventArgs)) return false;
+            var args = (MouseWheelEventArgs)inputEventArgs;
+            if (!base.Matches(targetElement, inputEventArgs))
+                return false;
+            if (Direction == MouseWheelDirection.Up && args.Delta > 0
+                || Direction == MouseWheelDirection.Down && args.Delta < 0)
+            {
+                inputEventArgs.Handled = true;
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+    
+}
